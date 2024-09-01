9661bb2cec7e244d77242c004e8adbb98d000447
dino <sjjeong1225@gmail.com>
2024-09-02 02:02:14 +0900

    초기 로딩 문구 추가

--- app/src/main/java/com/dino/lorempicsumexample/feature/photo_list/compose/PhotoListScreen.kt
+++ app/src/main/java/com/dino/lorempicsumexample/feature/photo_list/compose/PhotoListScreen.kt
@@ -2,16 +2,21 @@ package com.dino.lorempicsumexample.feature.photo_list.compose
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.dino.lorempicsumexample.feature.photo_list.PhotoListViewModel
@@ -47,6 +52,16 @@ private fun PhotoListScreen(
             modifier = modifier.padding(paddingValues = paddingValues),
             contentPadding = PaddingValues(4.dp),
         ) {
+            if (items.loadState.refresh == LoadState.Loading) {
+                item {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.CenterHorizontally),
+                        text = "데이터 불러오는 중..."
+                    )
+                }
+            }
             items(
                 count = items.itemCount,
                 key = { items[it]?.id ?: it }
@@ -54,6 +69,16 @@ private fun PhotoListScreen(
                 val item = items[index]
                 if (item != null) {
                     PhotoListItem(item = item)
+                }
+            }
+
+            if (items.loadState.append == LoadState.Loading) {
+                item {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.CenterHorizontally)
+                    )
                 }
             }
         }
